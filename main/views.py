5ccf8af0efb0578b6b61d8b9f4f1ff8433cf6ac6
clarista <clarista05@gmail.com>
2023-09-27 15:27:26 +0700

    selesai mengerjakan tutorial 4

--- main/views.py
+++ main/views.py
@@ -98,12 +98,31 @@ def login_user(request):
     context = {}
     return render(request, 'login.html', context)
 
-# def logout_user(request):
-#     logout(request)
-#     return redirect('main:login')
-
 def logout_user(request):
     logout(request)
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')
     return response
+
+def edit_product(request, id):
+    # Get product berdasarkan ID
+    product = Product.objects.get(pk = id)
+
+    # Set product sebagai instance dari form
+    form = ProductForm(request.POST or None, instance=product)
+
+    if form.is_valid() and request.method == "POST":
+        # Simpan form dan kembali ke halaman awal
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "edit_product.html", context)
+
+def delete_product(request, id):
+    # Get data berdasarkan ID
+    product = Product.objects.get(pk = id)
+    # Hapus data
+    product.delete()
+    # Kembali ke halaman awal
+    return HttpResponseRedirect(reverse('main:show_main'))
